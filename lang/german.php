b2bfc22571f0e0ceb93e277267d9e7a7dc901719
Gabriel Figueiro <info@netyson.com>
2020-04-22 18:00:18 +0200

    add translation "pro Monat"

---
+++ lang/german.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * WHMCS Language File
+ * German (de)
+ *
+ * Please Note: These language files are overwritten during software updates
+ * and therefore editing of these original files directly is not advised. Instead we
+ * recommend that you use overrides to customise the text displayed in a way
+ * which will be safely preserved through the upgrade process.
+ *
+ * For instructions on overrides, please visit:
+ *   https://developers.whmcs.com/languages/overrides/
+ *
+ * @package    WHMCS
+ * @author     WHMCS Limited <development@whmcs.com>
+ * @copyright  Copyright (c) WHMCS Limited 2005-2018
+ * @license    https://www.whmcs.com/license/ WHMCS Eula
+ * @version    $Id$
+ * @link       https://www.whmcs.com/
+ * 
+ * Dieses Sprachdatei ist zur Versionskontrolle abgeleget,
+ * nach Anpassung muss dies in das Verzeicbnlich "httpdocs / lang / overrides / german.php" ergänzt/ersetzt werden.
+ */
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+$_LANG['proMonat'] = "pro Monat";
+
+$_LANG['cartsubdomainchoice'] = "Subdomain von %s verwenden <b>(kostenlos)</b>";
+
+$_LANG['affiliatesactivate'] = "Aktiviere dein Partneraccount";
+$_LANG['affiliatesignuptitle'] = "Werden Sie unser Partner und beteiligen Sie sich an unserem Wachstum.";
+$_LANG['affiliatesignupintro'] = "Aktivieren Sie jetzt Ihren Partneraccount. Weitere Infos finden Sie unter <a href='https://arteeo.ch/partnerprogramm/'> arteeo.ch/partnerprogramm </a>";
+
+$_LANG['affiliatesignupinfo1'] = "Regelmässige und unkomplizierte Auszahlungen";
+$_LANG['affiliatesignupinfo2'] = "Wiederkehrende Zahlungen";
+$_LANG['affiliatesignupinfo3'] = "Attraktives Treueprogramm";
+
+$_LANG['affiliateslinktous'] = "Link zu unseren Produkten";
+
+$_LANG['unpaidInvoiceAlert'] = "Sie haben eine unbezahlte Rechnung. Bezahlen Sie die Rechnung jetzt, um eine Unterbrechung des Dienstes zu vermeiden.";
+$_LANG['overdueInvoiceAlert'] = "Sie haben eine überfällige Rechnung.";
+$_LANG['payInvoice'] = "Rechnung bezahlen";
+////////// End of german language file.  Do not place any translation strings below this line!
